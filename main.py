d512f401d69b5e269351548619afc3c876cb34c4
Guilherme_Carneiro <guilhermevinicius2313@gmail.com>
2024-08-29 21:53:34 -0300

    Config Inicial
    Criação de API com a biblioteca FASTAPI

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/teste")
+def teste_1():
+    return {"Mensagem": "Deu certo"}
